db4d8b5d5c054104d781a2c3afe92d54426e0fcf
adarabadian <adarabadian@gmail.com>
2022-11-10 19:53:28 +0200

    test

--- server/errors/errorHandler.js
+++ server/errors/errorHandler.js
@@ -2,7 +2,7 @@ const errorHandler = (error, request, response, next) => {
     console.log(error);
     if (error.errorType !== undefined && error.errorType.isShowStackTrace){
         console.log('aaa');
-        response.json(error);
+        response.json('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
         return;
     }
 
