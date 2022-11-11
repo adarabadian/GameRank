cd6c36953cee7dbd6335d4dc97e310f30c3a37c7
adarabadian <adarabadian@gmail.com>
2022-11-11 19:19:23 +0200

    Error handler fix

--- server/errors/errorHandler.js
+++ server/errors/errorHandler.js
@@ -1,10 +1,10 @@
 const errorHandler = (error, request, response, next) => {
     if (error.errorType !== undefined && error.errorType.isShowStackTrace){
-        response.status(error.errorType.httpCode).json({message: error.errorType.message});
+        response.status(error.errorType.httpCode).send(error.errorType.message);
         return;
     }
     
-    response.status(500).json({message: 'GENERAL ERROR OCCURED'});
+    response.status(500).send(error.errorType.message);
 }
 
 module.exports = errorHandler;
